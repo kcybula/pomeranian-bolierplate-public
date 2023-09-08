0c253a00b53dcae3f861e19762e986ec0d484174
Krzysiek C <krzysztof.cybula@pomeranianstartit.pl>
2023-09-08 21:39:08 +0200

    ArraysExercises

--- src/App/Exercises/Js/view-router-data.jsx
+++ src/App/Exercises/Js/view-router-data.jsx
@@ -3,10 +3,11 @@ import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 import { blockRouterMetaData as blockRouterMetaDataJsVariables1 } from './Exercise-js-variables-1/router-data';
 import { blockRouterMetaData as blockRouterMetaDataJsVariables2 } from './Exercise-js-variables-2/router-data';
 import { blockRouterMetaData as blockRouterMetaDataJsFunction1 } from './Exercise-js-functions/router-data';
-
+import { blockRouterMetaData as blockRouterMetaDataJsComplexTypes } from './Exercise-js-complex-types-in-js/router-data';
 export const blockRouterMetaData = [
   blockRouterMetaDataJsVariables1,
   blockRouterMetaDataJsVariables2,
   blockRouterMetaDataJsFunction1,
+  blockRouterMetaDataJsComplexTypes,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
