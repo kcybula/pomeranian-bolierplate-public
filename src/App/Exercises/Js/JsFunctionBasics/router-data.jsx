6f333eeb60367b0811a9bc7a3f6d8310ffca1a97
Krzysiek C <krzysztof.cybula@pomeranianstartit.pl>
2023-09-09 15:52:11 +0200

    JsFunctionExercise

---
+++ src/App/Exercises/Js/JsFunctionBasics/router-data.jsx
@@ -0,0 +1,10 @@
+import { JsFunctionBasics } from './JsFunctionBasics';
+
+export const blockRouterMetaData = {
+  path: 'js-function-basics',
+  date: '2023-09-09',
+  linkLabel: 'Funkcje podstawy - powtórka',
+  blockNo: 6,
+  element: <JsFunctionBasics />,
+  tags: ['function', 'js'],
+};
--- src/App/Exercises/Js/view-router-data.jsx
+++ src/App/Exercises/Js/view-router-data.jsx
@@ -4,10 +4,17 @@ import { blockRouterMetaData as blockRouterMetaDataJsVariables1 } from './Exerci
 import { blockRouterMetaData as blockRouterMetaDataJsVariables2 } from './Exercise-js-variables-2/router-data';
 import { blockRouterMetaData as blockRouterMetaDataJsFunction1 } from './Exercise-js-functions/router-data';
 import { blockRouterMetaData as blockRouterMetaDataJsComplexTypes } from './Exercise-js-complex-types-in-js/router-data';
+import { blockRouterMetaData as blockRouterMetaDataJsFunctionBasics } from './JsFunctionBasics/router-data';
+import { blockRouterMetaData as blockRouterMetaDataBlok22Warmup } from './Blok22Warmup/router-data';
+import { blockRouterMetaData as blockRouterMetaDataSetTimeout } from './SetTimeout/router-data';
+
 export const blockRouterMetaData = [
   blockRouterMetaDataJsVariables1,
   blockRouterMetaDataJsVariables2,
   blockRouterMetaDataJsFunction1,
   blockRouterMetaDataJsComplexTypes,
+  blockRouterMetaDataJsFunctionBasics,
+  blockRouterMetaDataBlok22Warmup,
+  blockRouterMetaDataSetTimeout,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
