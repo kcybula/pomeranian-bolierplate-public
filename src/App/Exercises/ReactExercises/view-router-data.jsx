5805ccc271a33bd80d43b46a9cfcfe9a98da7079
Krzysiek C <krzysztof.cybula@pomeranianstartit.pl>
2023-09-30 13:29:35 +0200

    ToDoWithServerHTTPExercise

--- src/App/Exercises/ReactExercises/view-router-data.jsx
+++ src/App/Exercises/ReactExercises/view-router-data.jsx
@@ -5,12 +5,14 @@ import { ReactEventsUseStateMetaData } from './ReactEventsUseState/router-data';
 import { ReactIfStatementsMetaData } from './ReactIfStatements/router-data';
 import { ReactIfStatementsMoreOrLessMetaData } from './ReactIfStatementsMoreOrLess/router-data';
 import { RouteErrorPrototypeThis } from './ErrorsPrototypesThis/router-data';
+import { ToDoWithServerMetaData } from './ToDoWithServer/router-data';
 export const blockRouterMetaData = [
   SubRouteExampleMetaData,
   ReactEventsUseStateMetaData,
   ReactIfStatementsMetaData,
   ReactIfStatementsMoreOrLessMetaData,
   RouteErrorPrototypeThis,
+  ToDoWithServerMetaData,
 ];
 
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
